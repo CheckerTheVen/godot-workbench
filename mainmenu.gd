aaa8447ad38dd1c3bdc1202e5a1051fd3b6d0623
Checklet <ac.cekic@outlook.com>
2025-06-02 13:02:27 +0200

    Implemented: Closing application when using exit button

--- mainmenu.gd
+++ mainmenu.gd
@@ -8,6 +8,8 @@ func _on_options_gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton and event.is_pressed():
         pass # TODO Open options menu (WIP)
 
+# Notifies all nodes of the upcoming termination of the program.
 func _on_exit_gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton and event.is_pressed():
-        pass # TODO Close application
+        get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+        get_tree().quit(0)
